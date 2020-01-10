7f8ac98ac6f575cd51094fb31a50b6e9fcaee9b7
Tymko Tecca <ttecca@seatgeek.com>
2020-01-10 13:08:14 -0500

    Steal/tweak AirBnB hooks rules

--- rules/react-hooks.js
+++ rules/react-hooks.js
@@ -1,4 +1,12 @@
 module.exports = {
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+	},
+	plugins: [
+		'react-hooks',
+	],
 	rules: {
 		'react-hooks/exhaustive-deps': 'warn',
 		'react-hooks/rules-of-hooks': 'error',
